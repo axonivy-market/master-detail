dfdbb670785db0a94f50bb3ceca2cf1a4c5078d2
fotini.kalantidou <fotini.kalantidou@axonivy.com>
2023-06-02 14:15:17 +0200

    implemented navigation

---
+++ master-detail-demo/src/com/axonivy/demo/masterdetail/enums/Availability.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.axonivy.demo.masterdetail.enums;
+
+/**
+ * @author david.merunko
+ *
+ */
+public enum Availability implements HasCmsName {
+	UNAVAILABLE,
+	IN_ORDER,
+	IN_STOCK;
+}
